b17dd86283eedd44f326aaa7a070ea6da0cc56b9
Eric Inman <ericrinman@gmail.com>
2022-12-10 16:35:30 -0800

    I dunno

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-// grab DOM elements
+// const nameDisplay = document.getElementById('name-display');
+// nameDisplay.textContent = 'Bruce';
+// nameDisplay.style.color = 'red';
 
-// set event listeners
+// const nametag = document.getElementById('nametag');
+// nametag.style.backgroundColor = 'purple';
+
+const button = document.getElementById('update-button');
+const nameDisplay = document.getElementById('name-display');
+const nameInput = document.getElementById('name-input');
+button.addEventListener('click', () => {
+    console.log('I am clicking the button');
+    console.log(nameDisplay);
+    nameDisplay.textContent = nameInput.value;
+})
